87e136068f196cf83fb4ffb52a0afcc06431dd71
JohnnKennedyy <john.238909@fmm.org.br>
2025-06-11 22:16:11 -0400

    Sobre

--- dse2/src/pages/Sobre.tsx
+++ dse2/src/pages/Sobre.tsx
@@ -2,6 +2,7 @@
 
 import Menu from '@/components/Menu';
 import FuncionarioButton from '@/components/Buttons/FuncionarioButton'
+import FuncionarioImagem from '@/assets/Funcionariosimg.png'
 
 export default function Sobre() {
     return (
@@ -11,9 +12,14 @@ export default function Sobre() {
             </div>
 
             <div className='flex flex-col w-4/5 border-2 items-center'>
-                <div className='w-4/5 bg-white m-5'>
-                    <h1>Sistema de Funcionários</h1>
-                    <h1>Funcionários</h1>
+                <div className='w-4/5 bg-white m-5 flex flex-row'>
+                    <div className='w-2/3 font-bold text-black text-2xl'>
+                        <h1>Histórico de Funcionários</h1>
+                    </div>
+
+                    <div className='w-1/3'>
+                        <img src={FuncionarioImagem.src} alt="" />
+                    </div>
                 </div>
 
                 <div className='w-4/5'>
