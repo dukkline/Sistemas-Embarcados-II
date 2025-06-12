a8846a9a5312c3ed64b7dc21ec8a7f77cc8fe410
JohnnKennedyy <john.238909@fmm.org.br>
2025-06-11 21:20:33 -0400

    Sobre

--- dse2/src/pages/Sobre.tsx
+++ dse2/src/pages/Sobre.tsx
@@ -3,20 +3,28 @@
 import Menu from '@/components/Menu';
 import FuncionarioButton from '@/components/Buttons/FuncionarioButton'
 
-export default function Sobre(){
-    return(
-        <div className="bg-purple-gradient h-screen flex">
-            <Menu/>
-            <div>
-                <h1>Sistema de Funcionários</h1>
-                <h1>Funcionários</h1>
+export default function Sobre() {
+    return (
+        <div className="bg-purple-gradient h-screen flex flex-row">
+            <div className='border-2 mr-20'>
+                <Menu />
             </div>
-            <FuncionarioButton
-                nome="John Kennedy"
-                funcao="Gerente"
-                // imagem=""
-                horario="25:99"
-                onclick={()=>{}}/>
+
+            <div className='flex flex-col w-4/5 border-2 items-center'>
+                <div className='w-4/5 bg-white m-5'>
+                    <h1>Sistema de Funcionários</h1>
+                    <h1>Funcionários</h1>
+                </div>
+
+                <div className='w-4/5'>
+                    <FuncionarioButton
+                        nome="John Kennedy"
+                        funcao="Gerente"
+                        // imagem=""
+                        horario="25:99"
+                        onclick={() => { }} />
+                </div>
+            </div>
         </div>
     );
 }
