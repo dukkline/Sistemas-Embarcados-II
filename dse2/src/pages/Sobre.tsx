391c91bd4f78ea9548503c7dec80f56554e925e9
JohnnKennedyy <john.238909@fmm.org.br>
2025-06-11 23:25:18 -0400

    Sobre

--- dse2/src/pages/Sobre.tsx
+++ dse2/src/pages/Sobre.tsx
@@ -8,8 +8,11 @@ import { initializeApp } from 'firebase/app';
 import { firebaseConfig } from './Firebase';
 import router from 'next/router';
 
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
 export default function Sobre() {
-    
+
     return (
         <div className="bg-purple-gradient h-screen flex flex-row">
             <div className='mr-20'>
@@ -17,9 +20,9 @@ export default function Sobre() {
             </div>
 
             <div className='flex flex-col w-4/5 items-center'>
-                <div className='w-4/5 bg-white m-5 flex flex-row rounded-2xl shadow-xl/30'>
-                    <div className='w-2/3 font-bold text-gray-800 text-4xl flex justify-center items-center wrap radious-xl'>
-                        <h1>histórico de funcionários</h1>
+                <div className='w-4/5 bg-white m-5 flex justify-around flex-row rounded-2xl shadow-xl/30'>
+                    <div className='w-1/3 ml-40 font-bold italic text-gray-800 text-4xl flex justify-center items-center text-wrap radious-xl'>
+                        <h1>Histórico de Funcionários</h1>
                     </div>
 
                     <div className='w-1/3 flex justify-center items-center'>
@@ -27,13 +30,18 @@ export default function Sobre() {
                     </div>
                 </div>
 
-                <div className='w-4/5'>
-                    <FuncionarioButton
-                        nome="John Kennedy"
-                        funcao="Gerente"
-                        // imagem=""
-                        horario="25:99"
-                        onclick={() => {set(ref(database, 'reconhecimento'), 'johnkennedy'); router.push('/sensoriamento')}} />
+                <div className='w-4/5 bg-indigo-950 rounded-2xl'>
+                    <div className='text-3xl italic font-bold text-white pl-5 pt-5'>
+                        <h1>Funcionários</h1>
+                    </div>
+                    <div className='p-5'>
+                        <FuncionarioButton
+                            nome="John Kennedy"
+                            funcao="Gerente"
+                            // imagem=""
+                            horario="25:99"
+                            onclick={() => { set(ref(database, 'reconhecimento'), 'johnkennedy'); router.push('/sensoriamento') }} />
+                    </div>
                 </div>
             </div>
         </div>
